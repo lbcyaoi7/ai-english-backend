8e0da2f6b5ed513da0df3c17f715413bcd3c4026
lbcyaoi7 <150247812+lbcyaoi7@users.noreply.github.com>
2026-04-25 14:39:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -129,4 +129,6 @@ async def http_exception_handler(request, exc):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
+    import os
+    port = int(os.getenv("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
